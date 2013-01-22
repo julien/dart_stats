44c26eae88e8489158153a7e60534404e7a4ed3c
Julien Castelain <jcastelain@gmail.com>
2013-01-22 14:05:22 +0100

    moved run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-PATH=$HOME/local/dart/chromium:$PATH
-results='DumpRenderTree test/index.html 2>&1'
-
-echo "$results" | grep CONSOLE
-echo $results | grep -v 'Exception: Some tests failed.' >/dev/null
