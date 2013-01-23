ebdd5c2710e542ded72f3080459d3be36d3dd022
Julien Castelain <jcastelain@gmail.com>
2013-01-23 19:46:58 +0100

    updated run.sh

--- test/run.sh
+++ test/run.sh
@@ -2,8 +2,27 @@
 
 set -e
 
-PATH=$DART_SDK/chromium:$PATH
-results='DumpRenderTree index.html 2>&1'
+#####
+# Unit Tests
+echo "DumpRenderTree test/index.html"
+results=`DumpRenderTree test/index.html 2>&1`
 
 echo "$results" | grep CONSOLE
+echo $results | grep 'unittest-suite-success' >/dev/null
 echo $results | grep -v 'Exception: Some tests failed.' >/dev/null
+
+#####
+# Type Analysis
+echo
+echo "dart_analyzer lib/*.dart"
+
+results=`dart_analyzer lib/*.dart 2>&1`
+
+echo "$results"
+
+if [ -n "$results" ]; then
+    exit 1
+else
+    echo "Passed analysis."
+fi
+
